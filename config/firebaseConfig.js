b068a4ca0dab7357870c7941b49c427b8199dd75
shad <shadahmor007@gmail.com>
2025-12-11 15:36:42 +0530

    bug fixes

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,6 +3,24 @@ const serviceAccount = require('../serviceAccountKey.json'); // Your Firebase se
 const retry = require('retry');
 const operation = retry.operation();
 const {decryptedData} = require('../utils/decryptUtils')
+
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE, // इसे env var से पढ़ें
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  // 🚨 महत्वपूर्ण: private_key को Git से बचने के लिए Env Var से पढ़ें
+  private_key: process.env.FIREBASE_PRIVATE_KEY 
+               ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') // \n को असली लाइन ब्रेक से बदलें
+               : undefined, 
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 // Initialize Firebase Admin SDK
 operation.attempt(function(currentAttempt) {
   try {
